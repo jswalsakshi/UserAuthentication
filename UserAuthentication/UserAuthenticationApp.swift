afeba71fbfa0b44b0f0cb9fbbd5c60418d8f7016
jswalsakshi <my@pers.on.al>
2024-01-26 07:59:15 +0530

    Initial Commit

---
+++ UserAuthentication/UserAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAuthenticationApp.swift
+//  UserAuthentication
+//
+//  Created by sakshi.j on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct UserAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
